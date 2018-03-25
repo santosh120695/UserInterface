d33693ec952f08c0807b401850a22c3214c72810
1206santosh <santosh@linkwok.com>
2018-03-25 15:14:06 +0530

    first commit

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+
+  def bootstrap_class_for flash_type
+    case flash_type.to_sym
+      when :success
+        "success"
+      when :error
+        "error"
+      when :alert
+        "warning"
+      when :notice
+        "info"
+      else
+        flash_type.to_s
+    end
+  end
 end
