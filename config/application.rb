56618eb289cfaa72e8ddda3a12804e0e6b99fa2f
1206santosh <santosh@linkwok.com>
2018-03-25 18:50:26 +0530

    assets serve set true

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,7 @@ module UserService
     config.load_defaults 5.1
     config.active_job.queue_adapter = :sidekiq
     config.serve_static_assets = true
+    config.public_file_server.enabled
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
